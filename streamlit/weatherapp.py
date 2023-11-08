ffabf51b87feffb1b007d90f221fbf4a2ce00bea
OlofStenlund <olof.stenlund@hotmail.com>
2023-11-08 20:35:24 +0100

    restructured strreamlit

--- streamlit/weatherapp.py
+++ streamlit/weatherapp.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-nav = st.sidebar.radio('Huvudmeny', ['Bakgrund', 'Frågeställning', 'EDA', 'Platsinformation', 'Vind', 'Snödjup', 'Nederbörd', 'Temperatur', 'Relationen mellan nederbörd & temperatur', 'Sommarens och vinterns ankomst'])
+nav = st.sidebar.radio('Huvudmeny', ['Bakgrund', 'Frågeställning', 'Platsinformation', 'Vind', 'Snödjup', 'Nederbörd', 
+                                     'Temperatur', 'Relationen mellan nederbörd & temperatur', 'Slutsatser'])
 
 if nav == 'Bakgrund':
 
@@ -17,70 +18,10 @@ if nav == 'Bakgrund':
             \nKan vi förutspå hur vädret ser ut i till exempel juli 2028?''')
 
 
-if nav == 'EDA':
+# if nav == 'EDA':
     # st.session_state
-    st.title('Lol, fat chance')
-    st.text('Välj månad att se:')
-    df = pd.read_pickle('../dataframes/df_compiled_adjusted_monthly_temp_gbg_save.pkl')
-    first_year = df['Year'].min()
-    last_year = df['Year'].max()
-    
-    col1, col2, col3 = st.columns(3)
 
-    res_January = col1.button('Januari')
-    res_February = col1.button('Febuari')
-    res_March = col1.button('Mars')
-    res_April = col1.button('April')
-    res_May = col2.button('Maj')
-    res_June = col2.button('Juni')
-    res_July = col2.button('Juli')
-    res_August = col2.button('Augusti')
-    res_September = col3.button('September')
-    res_October = col3.button('Oktober')
-    res_November = col3.button('November')
-    res_December = col3.button('December')    
 
-    month_list = ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December']
-    res_list = [res_January, res_February, res_March, res_April, res_May, res_June, res_July, res_August, res_September, res_October, res_November, res_December]
-    res_list_str = ['res_Januari', 'res_Februari', 'res_Mars', 'res_April', 'res_Maj', 'res_Juni', 'res_Juli', 'res_Augusti', 'res_September', 'res_Oktober', 'res_November', 'res_December']
-
-    
-
-    for ind, val in enumerate(res_list):
-        if val == True:
-            button = (f'res_{month_list[ind]}')
-            st.session_state.last_button = button
-        elif 'last_button' not in st.session_state:
-            st.session_state.last_button = None
-
-    desired_start, desired_end = st.slider('Välj tisdsperiod (om du känner för det):', min_value=first_year, max_value=last_year, value=[first_year, last_year])
-    desired_start = int(desired_start)
-    desired_end = int(desired_end)
-    
-    st.session_state.desired_start = desired_start
-    st.session_state.desired_end = desired_end
-
-    button = st.session_state.last_button
-    start = st.session_state.desired_start
-    end = st.session_state.desired_end
-
-    if st.session_state.last_button == None:
-        pass
-    else:
-        df = df[(df['Year'] >= start) & (df['Year'] <= end)]
-        for ind, val in enumerate(res_list_str):
-            if val == button:
-                month = ind
-
-        df = df[df['Month'] == month+1]
-        fig, ax = plt.subplots()
-        sns.scatterplot(data=df, x='Year', y='Snittemperatur')
-        sns.regplot(x=df['Year'], y=df['Snittemperatur'], ci=False)
-        # ax.scatter(x=df['Year'], y=df['Snittemperatur'])
-        ax.set_title(f'Temperaturer i {month_list[month]}')
-        ax.set_xlabel('År')
-        ax.set_ylabel('Snittemperatur per dag, Celcius')
-        st.pyplot(fig)
 
 
 if nav == 'Platsinformation':
@@ -100,7 +41,7 @@ if nav == 'Nederbörd':
 if nav == 'Vind':
     #### Overview
 
-    st.title('Överblick över vind-data ')
+    st.title('Överblick över vind-data')
 
     st.write('"Den vindhastighet som meteorologen anger i prognoser och flertalet av våra mätningar avser ett medelvärde under 10 minuter av vindhastigheten på 10 meters höjd ovan markytan." /SMHI')
     # st.write("""Som i de andra exempel har vi konkatenerat Säve och Göteborgs data för att kunna ha data från 1944 till 2023. Med knapparna nedan kan man skrolla igenom 
@@ -315,21 +256,94 @@ if nav == 'Temperatur':
     st.write('Temperaturer justerat efter månadssnitten ovan.')
     st.image(medeltemp)
 
-
-
-if nav == 'Sommarens och vinterns ankomst':
-    st.title('Sommarens och vinterns ankomst')
+    '\n'
+    '\n'
+    '\n'
     st.write('"Den meteorologiska definitionen av sommar är att dygnsmedeltemperaturen varaktigt ska vara minst 10,0°C." /SMHI')
     st.write('"Meteorologer definierar vinter som den period då dygnets medeltemperatur varaktigt är 0,0 grader eller lägre." /SMHI')
-    '\n'
-    '\n'
-    '\n'
-    '\n'
-    st.title('Sommarens ankomst')
-    summer_arrival = ('../plottar/summer_arrivals.png')
-    st.image(summer_arrival)
-    df = pd.read_pickle('../Dataframes/summer_days_diff.pkl')
-    st.table(df)
-    st.write('I snitt skiljer det 4.8 dagar mellan justerad och ojusterad temperatur.')
     summer_arrival_trendline = ('../plottar/summer_arrivals_trendline.png')
     st.image(summer_arrival_trendline, caption='I snitt ankommer sommaren 5e Maj')
+
+
+    #### Just for fun, see temps
+
+    st.title('Se temperaturer för månad och tidspann.')
+    '\n'
+    '\n'
+    df = pd.read_pickle('../dataframes/df_compiled_adjusted_monthly_temp_gbg_save.pkl')
+    first_year = df['Year'].min()
+    last_year = df['Year'].max()
+    
+    col1, col2, col3 = st.columns(3)
+
+    res_January = col1.button('Januari')
+    res_February = col1.button('Febuari')
+    res_March = col1.button('Mars')
+    res_April = col1.button('April')
+    res_May = col2.button('Maj')
+    res_June = col2.button('Juni')
+    res_July = col2.button('Juli')
+    res_August = col2.button('Augusti')
+    res_September = col3.button('September')
+    res_October = col3.button('Oktober')
+    res_November = col3.button('November')
+    res_December = col3.button('December')    
+
+    month_list = ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December']
+    res_list = [res_January, res_February, res_March, res_April, res_May, res_June, res_July, res_August, res_September, res_October, res_November, res_December]
+    res_list_str = ['res_Januari', 'res_Februari', 'res_Mars', 'res_April', 'res_Maj', 'res_Juni', 'res_Juli', 'res_Augusti', 'res_September', 'res_Oktober', 'res_November', 'res_December']
+
+    
+
+    for ind, val in enumerate(res_list):
+        if val == True:
+            button = (f'res_{month_list[ind]}')
+            st.session_state.last_button = button
+        elif 'last_button' not in st.session_state:
+            st.session_state.last_button = None
+
+    desired_start, desired_end = st.slider('Välj tisdsperiod (om du känner för det):', min_value=first_year, max_value=last_year, value=[first_year, last_year])
+    desired_start = int(desired_start)
+    desired_end = int(desired_end)
+    
+    st.session_state.desired_start = desired_start
+    st.session_state.desired_end = desired_end
+
+    button = st.session_state.last_button
+    start = st.session_state.desired_start
+    end = st.session_state.desired_end
+
+    if st.session_state.last_button == None:
+        pass
+    else:
+        df = df[(df['Year'] >= start) & (df['Year'] <= end)]
+        for ind, val in enumerate(res_list_str):
+            if val == button:
+                month = ind
+
+        df = df[df['Month'] == month+1]
+        fig, ax = plt.subplots()
+        sns.scatterplot(data=df, x='Year', y='Snittemperatur')
+        sns.regplot(x=df['Year'], y=df['Snittemperatur'], ci=False)
+        # ax.scatter(x=df['Year'], y=df['Snittemperatur'])
+        ax.set_title(f'Temperaturer i {month_list[month]}')
+        ax.set_xlabel('År')
+        ax.set_ylabel('Snittemperatur per dag, Celcius')
+        st.pyplot(fig)
+
+# if nav == 'Sommarens och vinterns ankomst':
+#     st.title('Sommarens och vinterns ankomst')
+#     st.write('"Den meteorologiska definitionen av sommar är att dygnsmedeltemperaturen varaktigt ska vara minst 10,0°C." /SMHI')
+#     st.write('"Meteorologer definierar vinter som den period då dygnets medeltemperatur varaktigt är 0,0 grader eller lägre." /SMHI')
+#     '\n'
+#     '\n'
+#     '\n'
+#     '\n'
+#     st.title('Sommarens ankomst')
+#     summer_arrival = ('../plottar/summer_arrivals.png')
+#     st.image(summer_arrival)
+#     df = pd.read_pickle('../Dataframes/summer_days_diff.pkl')
+#     st.table(df)
+#     st.write('I snitt skiljer det 4.8 dagar mellan justerad och ojusterad temperatur.')
+#     summer_arrival_trendline = ('../plottar/summer_arrivals_trendline.png')
+#     st.image(summer_arrival_trendline, caption='I snitt ankommer sommaren 5e Maj')
